ea2faeb88d2b6ddc98a9a10c760574dca993673c
armundle <amundle@cognitivescale.com>
2016-02-04 16:58:18 -0600

    change func1 to accept args

--- py/decorator_ex.py
+++ py/decorator_ex.py
@@ -10,32 +10,36 @@ def entryExitFunc(f):
 
 class entryExit(object):
 
-    def __init__(self, f):
     '''
     If there are no decorator arguments, the function to be decorated is passed
     to the constructor.
     '''
+    def __init__(self, f):
         self.f = f
+        print "entryExit.__init__"
 
     '''
-    The major constraint on the result of a decorator is that it be callable.
+    Note: The major constraint on the result of a decorator is that it be callable.
     The __call__ method here achieves that.
     '''
     '''
     The __call__ method is not called until the decorated function is called.
     '''
-    def __call__(self):
+    def __call__(self, *args):
+        print "entryExit.__call__"
         print "entering", self.f.__name__
-        self.f()
+        self.f(*args)
         print "exited", self.f.__name__
 
 @entryExit
-def func1():
+def func1(a1, a2, a3):
     print "inside function 1"
+    print "spell args: ", a1, a2, a3
 
 @entryExit
 def func2():
     print "inside function 2"
+    print "no args"
 
 @entryExitFunc
 def func3():
@@ -44,8 +48,13 @@ def func3():
 
 if __name__ == "__main__":
 
-    func1()
+    func1("test", "multiple", "args")
+    print '\n'
+    func1("another", "round", "of args")
     print '\n'
     func2()
     print '\n'
     func3()
+
+    print '\n'
+    print "end of example"
