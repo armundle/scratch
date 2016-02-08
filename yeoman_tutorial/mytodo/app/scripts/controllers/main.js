b47371106ce0e35aaa68b7b1273b8cb80ea1bc95
armundle <amundle@cognitivescale.com>
2016-02-08 12:59:02 -0600

    add persistence to todo list

--- yeoman_tutorial/mytodo/app/scripts/controllers/main.js
+++ yeoman_tutorial/mytodo/app/scripts/controllers/main.js
@@ -8,16 +8,25 @@
  * Controller of the mytodoApp
  */
 angular.module('mytodoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [];
+  .controller('MainCtrl', function($scope, localStorageService)
+              {
+              var todosInStore = localStorageService.get('todos');
 
-    $scope.addTodo = function() {
-    $scope.todos.push($scope.todo);
-    $scope.todo = '';
-    };
+              $scope.todos = todosInStore || [];
 
-    $scope.removeTodo = function (index)
-    {
-       $scope.todos.splice(index, 1);
-    };
+              $scope.$watch('todos', function()
+                               {
+                                  localStorageService.set('todos', $scope.todos);
+                               }, true);
+
+              $scope.addTodo = function()
+              {
+                 $scope.todos.push($scope.todo);
+                 $scope.todo = '';
+              };
+
+              $scope.removeTodo = function (index)
+              {
+                 $scope.todos.splice(index, 1);
+              };
   });
