9237773ca05da6c3ec28036a3ab926abf2f68d81
armundle <amundle@cognitivescale.com>
2016-04-30 22:26:33 -0500

    Add exercise 3.5

--- scala/3_5_ex.scala
+++ scala/3_5_ex.scala
@@ -3,11 +3,9 @@ case object Nil extends List[Nothing]
 case class Cons[A](head: A, tail: List[A]) extends List[A]
 
 object List{
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
-    case Nil => Nil
-    case Cons(head, tail) => {
-      if (f(head) == true) dropWhile(tail, f)
-      else tail
+  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] =
+    l match {
+      case Cons(h, t) if f(h) => dropWhile(t)(f)
+      case _ => l
     }
-  }
 }
