a3c4bf7fa5a303a94b160f2e75082cadbda55839
armundle <amundle@cognitivescale.com>
2016-05-01 15:16:50 -0500

    Add exercises in a single file

---
+++ scala/workbook.scala
@@ -0,0 +1,69 @@
+package fpinscala.datastructures.workbook
+
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List{
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def product(p: List[Double]) =
+    foldRight(p, 1.0)((x, y) => x * y)
+
+  def sum(s: List[Int]) =
+    foldRight(s, 0.0)((x, y) => x + y)
+
+  def length[A](as: List[A]): Int = 
+    foldRight(as, 0)((_, acc) => (acc + 1))
+
+  @annotation.tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B =
+    as match {
+      case Nil => z
+      case Cons(h, t) => foldLeft(t, f(z, h))(f)
+    }
+
+  def productFL(p: List[Double]): Double =
+    foldLeft(p, 1.0)((x, y) => x * y)
+
+  def sumFL(s: List[Int]): Int =
+    foldLeft(s, 0)((x, y) => x + y)
+
+  def lengthFL(as: List[Int]): Int =
+    foldLeft(as, 0)((acc, _) => (acc + 1))
+
+  //def reverse(l: List[Int]): List[Int] =
+    //foldLeft(l, List[Int]())((h, acc) => Cons(acc, h))
+  
+  def append[A](l: List[A], r: List[A]): List[A] =
+    foldRight(l, r)(Cons(_, _))
+
+  def addOne(l: List[Int]): List[Int] =
+    foldRight(l, Nil:List[Int])((h, t) => Cons(h + 1, t))
+
+  def toString(l: List[Double]): List[String] =
+    foldRight(l, Nil:List[String])((h, t) => Cons(h.toString, t))
+
+  def map[A, B](as: List[A])(f: A => B): List[B] =
+    foldRight(as, Nil:List[B])((h, t) => Cons(f(h), t))
+
+  def filter[A](as: List[A])(f: A => Boolean): List [A] =
+    foldRight(as, Nil:List[A])((h, t) => if (f(h)) Cons(h, t) else t)
+
+  def pairwiseAdd(a: List[Int], b: List[Int]): List[Int] = (a, b) match {
+    case (Nil, _) => b
+    case (_, Nil) => a
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, pairwiseAdd(t1, t2))
+  }
+
+  def zipWith[A](a: List[A], b: List[A])(f: (A, A) => A): List[A] = (a, b) match {
+    case(Nil, _) => b
+    case(_, Nil) => a
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
+  }
+}
